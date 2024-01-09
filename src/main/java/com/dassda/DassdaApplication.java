5e107595a440f2903a5a9f8181e704483f3a26b5
ARProxy <hocci0222@kakao.com>
2024-01-09 17:06:03 +0900

    jenkins

---
+++ src/main/java/com/dassda/DassdaApplication.java
@@ -0,0 +1,13 @@
+package com.dassda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DassdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DassdaApplication.class, args);
+	}
+
+}
